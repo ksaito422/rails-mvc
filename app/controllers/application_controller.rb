733f6fa678808722cf00868deb2e26bcd4eb4c4f
ksaito <kitsit.39@gmail.com>
2023-07-11 08:41:04 +0900

    feat: 記事投稿画面へのアクセスに認可を実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   include AuthHelper
+  include Pundit::Authorization
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   rescue_from ActiveRecord::RecordNotFound, with: :url_not_found
 
   def index
@@ -11,19 +13,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    authorize Article
+
     @article = Article.new
   end
 
   def create
-    if !current_user.nil?
+    if current_user.nil?
+      render new
+    else
       Article.create(
         title: params[:article][:title],
         content: params[:article][:content],
         user_id: @current_user.id
       )
       redirect_to root_url
-    else
-      render new
     end
   end
 
@@ -31,5 +35,12 @@ class ArticlesController < ApplicationController
 
   def url_not_found
     response.status = 404
+  end
+
+  def authenticate_user!
+    unless session[:user_id]
+      flash[:alert] = 'You need to sign in or sign up before continuing.'
+      redirect_to login_path
+    end
   end
 end
