7aae2ba3f0bda67734c6429b9001fb9f8def7391
ksaito <kitsit.39@gmail.com>
2023-07-11 08:51:47 +0900

    feat: Article#newポリシーテストを実装する

---
+++ spec/policies/article_policy_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ArticlePolicy do
+  subject { described_class }
+
+  let(:user) { User.new }
+
+  permissions :new? do
+    it '未ログインの場合、アクセスを許可しない' do
+      expect(subject).not_to permit(nil, Article.new)
+    end
+
+    it 'ログインしている場合、アクセスを許可する' do
+      expect(subject).to permit(user, Article.new)
+    end
+  end
+end
