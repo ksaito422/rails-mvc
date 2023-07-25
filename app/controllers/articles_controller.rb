0bdb12bbcca3441e606e0ccb32174feeab90fd45
ksaito <kitsit.39@gmail.com>
2023-07-26 08:56:28 +0900

    feat: 更新画面への遷移を実装する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   # ログインしてsessionにuser_idを保持しているかチェックする
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # リソースが存在しない場合にhttp not foundを返す
   rescue_from ActiveRecord::RecordNotFound, with: :url_not_found
@@ -22,6 +23,8 @@ class ArticlesController < ApplicationController
 
   def edit
     authorize Article
+
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -65,5 +68,13 @@ class ArticlesController < ApplicationController
 
     flash[:alert] = 'You need to sign in or sign up before continuing.'
     redirect_to login_path
+  end
+
+  def correct_user
+    @article = Article.find(params[:id])
+    unless @article.user_id == current_user.id
+      flash[:alert] = "You don't have permission to do this."
+      redirect_to(root_url)
+    end
   end
 end
